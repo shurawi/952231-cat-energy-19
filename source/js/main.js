469e58f28113ec1fa5d0b57776df66494393b069
shurawi <denis.b.toropov@gmail.com>
2021-05-16 23:10:16 +0700

    добавляет в main.js состояния <было-стало>, вносит изменения для кнопки открытия-закрытия мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,26 @@
-let mobileBtns = document.querySelectorAll ('.header__item--white');
+let mobileBtns = document.querySelectorAll ('.header__list--menu .header__item');
 let openCloseBtn = document.querySelector('.header__btn');
+let afterBtn = document.querySelector('.example__label--after');
+let beforeBtn = document.querySelector('.example__label--before');
+let afterImg = document.querySelector('.example__img--after');
+let beforeImg = document.querySelector('.example__img--before');
 
+//adds open/close for header nav
 openCloseBtn.addEventListener ('click', function () {
   for (let mobileBtn of mobileBtns) {
     mobileBtn.classList.toggle('header__item--mobile');
   }
   openCloseBtn.classList.toggle('header__btn--open');
   openCloseBtn.classList.toggle('header__btn--close');
+})
+
+//adds before/after condition in example.scss
+afterBtn.addEventListener ('click', function () {
+  afterImg.classList.remove('example__img--hidden');
+  beforeImg.classList.add('example__img--hidden');
+})
+
+beforeBtn.addEventListener ('click', function () {
+  beforeImg.classList.remove('example__img--hidden');
+  afterImg.classList.add('example__img--hidden');
 })
